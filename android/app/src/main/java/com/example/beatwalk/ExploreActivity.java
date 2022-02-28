301c74ea579722d6fcc51e479e6a78a42a0f0142
hkwang24 <hannah.wang.k@gmail.com>
2022-02-28 10:11:29 -0500

    fixed splash page & added activities

---
+++ android/app/src/main/java/com/example/beatwalk/ExploreActivity.java
@@ -0,0 +1,14 @@
+package com.example.beatwalk;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ExploreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_explore);
+    }
+
+}
---
+++ android/app/src/main/java/com/example/beatwalk/PlayActivity.java
@@ -0,0 +1,14 @@
+package com.example.beatwalk;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PlayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play);
+    }
+
+}
