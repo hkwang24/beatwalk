a5bbd906b0c104623a66994fcd108880fa580e6c
lindseyyu <lindsyu@wharton.upenn.edu>
2022-04-15 20:25:21 -0400

    block visualizations for explore and play, assets added too

--- android/app/src/main/java/com/example/beatwalk/ExploreActivity.java
+++ android/app/src/main/java/com/example/beatwalk/ExploreActivity.java
@@ -3,6 +3,8 @@ package com.example.beatwalk;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,6 +14,60 @@ public class ExploreActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explore);
-    }
 
+
+        Button button= (Button)findViewById(R.id.update_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                char[] notes = {'c', 'b', ' ', 'a', ' ', ' ', 'e', 'f'};
+                char[] rhythm = {'q','h', ' ', 'h', ' ', 'r', 'q', 'q'};
+                int[] note_ids = {R.id.let_pos1, R.id.let_pos2, R.id.let_pos3, R.id.let_pos4, R.id.let_pos5, R.id.let_pos6, R.id.let_pos7, R.id.let_pos8};
+                int[] q_note_ids = {R.id.qnote_pos1, R.id.qnote_pos2, R.id.qnote_pos3, R.id.qnote_pos4, R.id.qnote_pos5, R.id.qnote_pos6, R.id.qnote_pos7, R.id.qnote_pos8};
+                int[] h_note_ids = {R.id.hnote_pos1, R.id.hnote_pos2, R.id.hnote_pos3, R.id.hnote_pos4, R.id.hnote_pos5, R.id.hnote_pos6, R.id.hnote_pos7};
+
+                for (int i = 0; i < notes.length; i++) {
+                    ImageView note_block = (ImageView)findViewById(note_ids[i]);
+
+                    if (notes[i] == 'a') {
+                        note_block.setImageResource(R.drawable.a_note);
+                        note_block.setVisibility(View.VISIBLE);
+                    } else if (notes[i] == 'b') {
+                        note_block.setImageResource(R.drawable.b_note);
+                        note_block.setVisibility(View.VISIBLE);
+                    } else if (notes[i] == 'c') {
+                        note_block.setImageResource(R.drawable.c_note);
+                        note_block.setVisibility(View.VISIBLE);
+                    } else if (notes[i] == 'd') {
+                        note_block.setImageResource(R.drawable.d_note);
+                        note_block.setVisibility(View.VISIBLE);
+                    } else if (notes[i] == 'e') {
+                        note_block.setImageResource(R.drawable.e_note);
+                        note_block.setVisibility(View.VISIBLE);
+                    } else if (notes[i] == 'f') {
+                        note_block.setImageResource(R.drawable.f_note);
+                        note_block.setVisibility(View.VISIBLE);
+                    } else if (notes[i] == 'g') {
+                        note_block.setImageResource(R.drawable.g_note);
+                        note_block.setVisibility(View.VISIBLE);
+                    }
+                }
+
+                for (int i = 0; i < rhythm.length; i++) {
+                    if (rhythm[i] == 'q') {
+                        ImageView rhy_block = (ImageView)findViewById(q_note_ids[i]);
+                        rhy_block.setImageResource(R.drawable.quarter_note);
+                        rhy_block.setVisibility(View.VISIBLE);
+                    } else if (rhythm[i] == 'r') {
+                        ImageView rhy_block = (ImageView) findViewById(q_note_ids[i]);
+                        rhy_block.setImageResource(R.drawable.rest_note);
+                        rhy_block.setVisibility(View.VISIBLE);
+                    } else if (rhythm[i] == 'h') {
+                        ImageView rhy_block = (ImageView) findViewById(h_note_ids[i]);
+                        rhy_block.setVisibility(View.VISIBLE);
+                    }
+                }
+            }
+        });
+    }
 }
--- android/app/src/main/java/com/example/beatwalk/SongOne.java
+++ android/app/src/main/java/com/example/beatwalk/SongOne.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,7 +22,61 @@ public class SongOne extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_song_one);
-    }
+        setContentView(R.layout.content_song_one);
 
+        Button button= (Button)findViewById(R.id.update_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                char[] notes = {'c', 'b', ' ', 'a', ' ', ' ', 'e', 'f'};
+                char[] rhythm = {'q','h', ' ', 'h', ' ', 'r', 'q', 'q'};
+                int[] note_ids = {R.id.let_pos1, R.id.let_pos2, R.id.let_pos3, R.id.let_pos4, R.id.let_pos5, R.id.let_pos6, R.id.let_pos7, R.id.let_pos8};
+                int[] q_note_ids = {R.id.qnote_pos1, R.id.qnote_pos2, R.id.qnote_pos3, R.id.qnote_pos4, R.id.qnote_pos5, R.id.qnote_pos6, R.id.qnote_pos7, R.id.qnote_pos8};
+                int[] h_note_ids = {R.id.hnote_pos1, R.id.hnote_pos2, R.id.hnote_pos3, R.id.hnote_pos4, R.id.hnote_pos5, R.id.hnote_pos6, R.id.hnote_pos7};
+
+                for (int i = 0; i < notes.length; i++) {
+                    ImageView note_block = (ImageView)findViewById(note_ids[i]);
+
+                    if (notes[i] == 'a') {
+                        note_block.setImageResource(R.drawable.a_note);
+                        note_block.setVisibility(View.VISIBLE);
+                    } else if (notes[i] == 'b') {
+                        note_block.setImageResource(R.drawable.b_note);
+                        note_block.setVisibility(View.VISIBLE);
+                    } else if (notes[i] == 'c') {
+                        note_block.setImageResource(R.drawable.c_note);
+                        note_block.setVisibility(View.VISIBLE);
+                    } else if (notes[i] == 'd') {
+                        note_block.setImageResource(R.drawable.d_note);
+                        note_block.setVisibility(View.VISIBLE);
+                    } else if (notes[i] == 'e') {
+                        note_block.setImageResource(R.drawable.e_note);
+                        note_block.setVisibility(View.VISIBLE);
+                    } else if (notes[i] == 'f') {
+                        note_block.setImageResource(R.drawable.f_note);
+                        note_block.setVisibility(View.VISIBLE);
+                    } else if (notes[i] == 'g') {
+                        note_block.setImageResource(R.drawable.g_note);
+                        note_block.setVisibility(View.VISIBLE);
+                    }
+                }
+
+                for (int i = 0; i < rhythm.length; i++) {
+                    if (rhythm[i] == 'q') {
+                        ImageView rhy_block = (ImageView)findViewById(q_note_ids[i]);
+                        rhy_block.setImageResource(R.drawable.quarter_note);
+                        rhy_block.setVisibility(View.VISIBLE);
+                    } else if (rhythm[i] == 'r') {
+                        ImageView rhy_block = (ImageView) findViewById(q_note_ids[i]);
+                        rhy_block.setImageResource(R.drawable.rest_note);
+                        rhy_block.setVisibility(View.VISIBLE);
+                    } else if (rhythm[i] == 'h') {
+                        ImageView rhy_block = (ImageView) findViewById(h_note_ids[i]);
+                        rhy_block.setVisibility(View.VISIBLE);
+                    }
+                }
+            }
+        });
+    }
 }
+
