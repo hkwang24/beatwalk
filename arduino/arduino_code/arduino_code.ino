d4a01504f970301d1003ec5ebecc2116a7a04713
hkwang24 <hannah.wang.k@gmail.com>
2022-04-22 21:32:26 -0400

    add a block to arduino

--- arduino/arduino_code/arduino_code.ino
+++ arduino/arduino_code/arduino_code.ino
@@ -35,10 +35,10 @@ void setup() {
   pinMode(12, INPUT_PULLUP);
 
   if (!sfx.reset()) {
-    Serial.println("Not found");
+//    Serial.println("Not found");
     while (1);
   } else {
-    Serial.println("SFX board found");
+//    Serial.println("SFX board found");
   }
 }
 
@@ -48,6 +48,7 @@ boolean looping = false;
 void loop() {
   checkNewBlocks();
   if (digitalRead(A1) == LOW) {
+      delay(800);
       int blocks[16];
       checkBlocks(blocks);
       sendData(blocks); 
@@ -55,6 +56,7 @@ void loop() {
       delay(1000);
   }
   while (looping) {
+      delay(800);
       int blocks[16];
       checkBlocks(blocks);
       sendData(blocks); 
@@ -92,6 +94,8 @@ char getValue(int i) {
     return 'b';
   } else if (735 <= i && i <= 845) {
     return 'f';
+   } else if (500 <= i && i <= 520) {
+    return 'a';
   } else {
     return ' ';
   }
@@ -151,10 +155,10 @@ void playNote (char note, boolean half) {
       break;
   }
   if (!played) {
-    Serial.println("Failed to play track");
-    Serial.println(note);
+//    Serial.println("Failed to play track");
+//    Serial.println(note);
   } else {
-    Serial.println(note);
+//    Serial.println(note);
   }
 }
 
@@ -169,7 +173,7 @@ void turnOffLED() {
 }
 
 void play(int blocks[]) {
-  Serial.println("Playing blocks");
+//  Serial.println("Playing blocks");
   char r;
   char p;
   boolean playedHalf = false;
@@ -289,7 +293,7 @@ void checkNewBlocks() {
     if (c == 'q' || c == 'h' || c == 'r') {
     } else if (c != ' ') {
       if (!block_found[i]) {
-        Serial.println("New block found");
+//        Serial.println("New block found");
       int ledNumb = 5 + (i / 2);
       if (ledNumb == 12) {
         digitalWrite(13, HIGH);
